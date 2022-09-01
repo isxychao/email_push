d8ae7c07968492a7b82deed5d40fe555240d704c
isxychao <50362316+isxychao@users.noreply.github.com>
2022-09-01 16:04:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 python -m pip install --upgrade pip
 pip3 install -r requirements.txt
 
+echo "Start scraping data"
 python3 ./get_data.py
+echo "Done"
 
 year=`date +%Y `
 month=`date +%m `
@@ -9,9 +11,12 @@ day=`date +%d `
 hour=`date +%H`
 now=$year-$month-$day-$hour
 
-
+echo "Sending Email"
 git config --global user.email "isxychao@outlook.com"
 git config --global user.name "actioner"
+echo "Done"
 
+echo "Pushing to github"
 git add .
 git commit -m "$now"
+echo "Done"
